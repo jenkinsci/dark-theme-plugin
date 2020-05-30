13eab3c2cfe2509d124bcb0f2f54d88ab41cbd41
Oleg Nenashev <o.v.nenashev@gmail.com>
2020-05-30 02:28:24 +0200

    Update the branch to purge cache

--- demo/enable-theme.groovy
+++ demo/enable-theme.groovy
@@ -12,7 +12,7 @@ boolean developerMode = Boolean.getBoolean("io.jenkins.themes.dark.developerMode
 // Simple Theme
 if (!disabled) {
     String themeUrl = developerMode
-        ? "https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login/theme.css"
+        ? "https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login2/theme.css"
         : "https://cdn.jsdelivr.net/gh/jenkinsci/dark-theme@master/theme.css"
     def simpleThemeConfig = Jenkins.instance.getExtensionList(PageDecorator.class).get(SimpleThemeDecorator.class)
     simpleThemeConfig.getElements().add(new CssUrlThemeElement(themeUrl))
@@ -35,9 +35,9 @@ if (developerMode) {
 //        <link rel="stylesheet" href="http://localhost:8080/userContent/login-theme/simple-page-forms.css" type="text/css" />
 //    '''
     loginThemeConfig.head = '''
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login/login-theme/simple-page.css" type="text/css" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login/login-theme/simple-page.theme.css" type="text/css" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login/login-theme/simple-page-forms.css" type="text/css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login2/login-theme/simple-page.css" type="text/css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login2/login-theme/simple-page.theme.css" type="text/css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login2/login-theme/simple-page-forms.css" type="text/css" />
     '''
 } else {
     loginThemeConfig.head = '''
@@ -49,7 +49,7 @@ if (developerMode) {
 
 if (!disabled) {
     if (developerMode) {
-        loginThemeConfig.head += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login/theme.css" type="text/css" />'
+        loginThemeConfig.head += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/oleg-nenashev/dark-theme@dark-login2/theme.css" type="text/css" />'
     } else {
         loginThemeConfig.head += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jenkinsci/dark-theme@master/theme.css" type="text/css" />'
     }
