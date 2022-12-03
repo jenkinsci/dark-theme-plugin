4a8eca0f304f9db29638b9606dc70a7885e5ebd0
Stefan Spieker <S.Spieker@gmx.net>
2022-12-03 10:29:25 +0100

    Removed unused imports (#302)

--- src/main/java/io/jenkins/plugins/darktheme/DarkThemeRootAction.java
+++ src/main/java/io/jenkins/plugins/darktheme/DarkThemeRootAction.java
@@ -1,28 +1,16 @@
 package io.jenkins.plugins.darktheme;
 
 import hudson.Extension;
-import hudson.Main;
 import hudson.Plugin;
-import hudson.PluginWrapper;
 import hudson.model.UnprotectedRootAction;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 import javax.servlet.ServletException;
 import jenkins.model.Jenkins;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
-import org.kohsuke.stapler.WebMethod;
 
 import static io.jenkins.plugins.darktheme.DarkThemeManagerFactory.THEME_CSS;
 import static io.jenkins.plugins.darktheme.DarkThemeManagerFactory.THEME_URL_NAME;
